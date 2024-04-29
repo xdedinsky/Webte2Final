e6143958f69c4ec9ea7a4193722e489d63472dfb
juraj <xlopusek@stuba.sk>
2024-04-29 16:43:15 +0200

    validacia hesla

--- src/script.js
+++ src/script.js
@@ -1,6 +1,18 @@
 function validatePassword() {
     var password = document.getElementById("passwordReg");
     var confirm_password = document.getElementById("confirm_password");
+    if (password.length < 6) {
+        Swal.fire({
+            title: 'Weak Password!',
+            text: 'Your password must be at least 6 characters long.',
+            icon: 'error',
+            background: '#FFFFFF',
+            color: '#000000',
+            confirmButtonColor: '#FF6A00',
+            confirmButtonText: 'Ok'
+        });
+        return false; // Prevent form submission
+    }
 
     if (password.value !== confirm_password.value) {
         Swal.fire({
