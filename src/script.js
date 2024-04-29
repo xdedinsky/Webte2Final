f7ce15e9b87b7b17a157639c52293cd52007e2cc
juraj <xlopusek@stuba.sk>
2024-04-29 17:12:36 +0200

    comit1

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 function validatePassword() {
-    var password = document.getElementById("passwordReg");
-    var confirm_password = document.getElementById("confirm_password");
-    if (password.length < 6) {
+    var password = document.getElementById("passwordReg").value;
+    var confirm_password = document.getElementById("confirm_password").value;
+    if (password.length < 6 || confirm_password.length < 6) {
         Swal.fire({
             title: 'Weak Password!',
             text: 'Your password must be at least 6 characters long.',
@@ -14,7 +14,7 @@ function validatePassword() {
         return false; // Prevent form submission
     }
 
-    if (password.value !== confirm_password.value) {
+    if (password !== confirm_password) {
         Swal.fire({
             title: 'Password Mismatch!',
             text: 'Please make sure your passwords match.',
