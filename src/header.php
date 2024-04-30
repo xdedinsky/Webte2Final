bc416d1457d9bc0b6c7229b1dc652108e14e3379
juraj <xlopusek@stuba.sk>
2024-04-30 16:12:08 +0200

    finalna valid. reg.

--- src/header.php
+++ src/header.php
@@ -95,19 +95,19 @@
                         <form action="controllers/register.php" method="post" onsubmit="return validatePassword();">
                             <div class="mb-3">
                                 <label for="username" localize="username" class="form-label"></label>
-                                <input type="text" class="form-control" id="username" name="username" required>
+                                <input type="text" class="form-control" id="username" name="username" >
                             </div>
                             <div class="mb-3">
                                 <label for="email" localize="email" class="form-label"></label>
-                                <input type="email" class="form-control" id="email" name="email" required>
+                                <input type="text" class="form-control" id="email" name="email" >
                             </div>
                             <div class="mb-3">
                                 <label for="password" localize="pwd" class="form-label"></label>
-                                <input type="password" class="form-control" id="passwordReg" name="passwordReg" required>
+                                <input type="password" class="form-control" id="passwordReg" name="passwordReg" >
                             </div>
                             <div class="mb-3">
                                 <label for="confirm_password" localize="confirm_pwd" class="form-label"></label>
-                                <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
+                                <input type="password" class="form-control" id="confirm_password" name="confirm_password" >
                             </div>
                             <button type="submit" localize="signup_button" class="btn btn-primary"></button>
                         </form>
