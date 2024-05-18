346f0c228461a345d68a7a0225aabd630fff331f
xtaborsky <martin.taborsky@tido.sk>
2024-05-18 11:48:29 +0200

    manual text

--- src/manual.php
+++ src/manual.php
@@ -11,7 +11,7 @@ include "header.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manuál pre administrátora</title>
+    <title localize="title"></title>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -58,11 +58,11 @@ include "header.php";
                 <div class="card" id="manual">
                     <div class="card-body">
                         <button id="downloadButton" onclick="generatePDF()" localize="download_info"></button>
-                        <h1 class="card-title" localize="sk_info">Manuál pre používateľa</h1>
+                        <h1 class="card-title" localize="manual_info"></h1>
                         <p class="card-text" localize="pdf"></p>
                         <p class="pManual">
                             <img src="images/backup.png" alt="backup" width="20" height="20" class="iconManual">
-                            <span localize="backup_q">Záloha</span>
+                            <span localize="back_up"></span>
                         </p>
                         <p class="pManual">
                             <img src="images/bin.png" alt="bin" width="20" height="20" class="iconManual">
@@ -93,137 +93,109 @@ include "header.php";
             <div class="col-md-6">
                 <div class="user mt-3">
                     <div class="title_user">
-                        <h1>Manuál pre používateľa</h1>
+                        <h1 localize="manual_info"></h1>
                     </div>
-                    <h2>Prihlásenie:</h2>
+                    <h2 localize="login"></h2>
                     <ul>
-                        <li>Používateľ sa môže prihlásiť pomocou vlastnej registrácie.</li>
+                        <li localize="text_1"></li>
                     </ul>
 
-                    <h2>Zmena hesla:</h2>
+                    <h2 localize="change_pwd_i"></h2>
                     <ul>
-                        <li>Používateľ môže zmeniť svoje heslo. Kliknutim na svoje meno v menu a v sekcii
-                            Nastavienie
-                            účtu</li>
+                        <li localize="text_2"></li>
                     </ul>
 
-                    <h2>Hlasovacie otázky:</h2>
-                    <h5>Hlasovanie je možne v menu Pridať otázku </h5>
+                    <h2 localize="vote_i"></h2>
+                    <h5 localize="text_3_i"></h5>
                     <ul>
-                        <li>Používateľ môže definovať viacero hlasovacích otázok.</li>
-                        <li>Používateľ môže definovať aktívne a neaktívne otázky.</li>
-                        <li>Každej otázke je generovaný QR kód a unikátny 5-znakový kód.</li>
+                        <li localize="text_4_i"></li>
+                        <li localize="text_5_i"></li>
+                        <li localize="text_6_i"></li>
                     </ul>
 
-                    <h2>Typy otázok:</h2>
+                    <h2 localize="text_7_i"></h2>
                     <ul>
-                        <li>Otázky s výberom správnej odpovede.</li>
-                        <li>Otázky s otvorenou krátkou odpoveďou.</li>
+                        <li localize="text_8_i"></li>
+                        <li localize="text_9_i"></li>
                     </ul>
 
-                    <h2>Zobrazenie výsledkov:</h2>
+                    <h2 localize="text_10_i"></h2>
                     <ul>
-                        <li>Pri otázkach s otvorenou odpoveďou je možné zobraziť výsledky ako zoznam alebo "word
-                            cloud".
-                        </li>
+                        <li localize="text_11_i"></li>
                     </ul>
 
-                    <h2>Úprava otázok:</h2>
+                    <h2 localize="text_12_i"></h2>
                     <ul>
-                        <li>Používateľ môže upravovať, mazať a kopírovať existujúce otázky.</li>
+                        <li localize="text_13_i"></li>
                     </ul>
 
-                    <h2>Vzťah k predmetom:</h2>
+                    <h2 localize="text_14_i"></h2>
                     <ul>
-                        <li>Každej otázke je možné priradiť predmet.</li>
+                        <li localize="text_15_i"></li>
                     </ul>
 
-                    <h2>Filtrovanie otázok:</h2>
+                    <h2 localize="text_16_i"></h2>
                     <ul>
-                        <li>Otázky je možné filtrovať podľa predmetu a dátumu vytvorenia.</li>
+                        <li localize="text_17_i"></li>
                     </ul>
 
-                    <h2>Uzatvorenie hlasovania:</h2>
+                    <h2 localize="text_18_i"></h2>
                     <ul>
-                        <li>Používateľ môže uzavrieť aktuálne hlasovanie na otázku, čo zahŕňa zazálohovanie odpovedí
-                            a
-                            pridanie
-                            poznámky a dátumu uzatvorenia.</li>
+                        <li localize="text_19_i"></li>
                     </ul>
 
-                    <h2>Zobrazenie výsledkov:</h2>
+                    <h2 localize="text_20_i"></h2>
                     <ul>
-                        <li>Možnosť zobraziť výsledky aktuálnych aj archivovaných hlasovaní.</li>
+                        <li localize="text_21_i"></li>
                     </ul>
 
-                    <h2>Porovnanie s historickými hlasovaniami:</h2>
+                    <h2 localize="text_22_i"></h2>
                     <ul>
-                        <li>Pri otázkach s výberom správnej odpovede je možné porovnať aktuálne hlasovanie s
-                            historickými
-                            pomocou tabuľky.</li>
+                        <li localize="text_23_i"></li>
                     </ul>
 
-                    <h2>Export otázok a odpovedí:</h2>
+                    <h2 localize="text_24_i"></h2>
                     <ul>
-                        <li>Možnosť exportu otázok a odpovedí do externých súborov (CSV, JSON, XML).</li>
+                        <li localize="text_25_i"></li>
                     </ul>
                 </div>
                 <div class="admin_text mt-3">
-                    <h1>Manuál pre admina</h1>
+                    <h1 localize="text_26_i"></h1>
                     <ol>
-                        <li>Administrátor má tú istú funkčnosť ako prihlásený používateľ s tým rozdielom, že má k
-                            dispozícii
-                            hlasovacie otázky všetkých prihlásených používateľov s možnosťou filtrovania nad
-                            vybraným
-                            používateľom.
-                        </li>
-                        <li>
-                            Pri vytváraní novej hlasovacej otázky administrátor môže špecifikovať, či to robí
-                            vlastným
-                            menom alebo v
-                            mene iného používateľa.
-                        </li>
-                        <li>
-                            Administrátor má prístup k správe prihlásených používateľov, vrátane vytvárania,
-                            čítania,
-                            aktualizácie a
-                            mazania (CRUD) používateľských účtov, ako aj možnosť zmeny ich rolí a hesiel.
-                        </li>
+                        <li localize="text_27_i"></li>
+                        <li localize="text_28_i"></li>
+                        <li localize="text_29_i"></li>
                     </ol>
                 </div>
                 <div class="mt-3">
-                    <h2>Získanie hlasovacej otázky:</h2>
+                    <h2 localize="text_30_i"></h2>
                     <ul>
-                        <li>Používateľ sa môže dostať na stránku s hlasovacou otázkou:</li>
+                        <li localize="text_31_i"></li>
                         <ul>
-                            <li>Načítaním zverejneného QR kódu.</li>
-                            <li>Zadaním vstupného kódu na domovskej stránke aplikácie.</li>
-                            <li>Zadaním adresy do prehliadača v tvare `https://nodeXX.webte.fei.stuba.sk/abcde`, kde
-                                `abcde` je 5-znakový vstupný kód.</li>
+                            <li localize="text_32_i"></li>
+                            <li localize="text_33_i"></li>
+                            <li localize="text_34_i"></li>
                         </ul>
                     </ul>
                 </div>
                 <div class="col">
-                    <h2>Vyplnenie hlasovacej otázky:</h2>
+                    <h2 localize="text_35_i"></h2>
                     <ul>
-                        <li>Po vyplnení hlasovacej otázky bude užívateľ presmerovaný na stránku s grafickým zobrazením
-                            výsledkov hlasovania na danú otázku.</li>
-                        <li>Na tejto stránke bude možný návrat na domovskú stránku aplikácie.</li>
+                        <li localize="text_36_i"></li>
+                        <li localize="text_37_i"></li>
                     </ul>
                 </div>
                 <div class="col">
-                    <h2>Zobrazenie výsledkov:</h2>
+                    <h2 localize="text_38_i"></h2>
                     <ul>
-                        <li>Pri zobrazení výsledkov hlasovania na otvorenú otázku budú odpovede zobrazené buď ako
-                            položky nečíslovaného zoznamu alebo pomocou tzv. "word cloud-u".</li>
-                        <li>Pri "word cloud-e" bude mať počet rovnakých odpovedí na otázku vplyv na veľkosť písma pri
-                            zobrazení tejto odpovede.</li>
+                        <li localize="text_39_i"></li>
+                        <li localize="text_40_i"></li>
                     </ul>
                 </div>
             </div>
         </div>
     </div>
-
+    <script src="script.js"></script>
     <!-- Bootstrap JS and other libraries -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.3.1/jspdf.umd.min.js"></script>
